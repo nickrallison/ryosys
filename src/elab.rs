673a234a8b7b968b41ef7630149925f6d6075f35
nick <nickrallison1@gmail.com>
2025-04-15 10:22:35 -0600

    modularizing

--- src/elab.rs
+++ src/elab.rs
@@ -1,11 +1,22 @@
+use std::os::raw::c_char;
+use crate::bindings::{new_yosys_rtlil_design, Yosys_RTLIL_Design, Yosys_run_frontend_wrapper, Yosys_yosys_setup};
 use crate::frontend::Frontend;
+use crate::helper::str_to_cstr;
 
 pub struct Elab {
-
+    yosys_rtlil_design: *mut Yosys_RTLIL_Design
 }
 
 impl Elab {
     pub fn elab(frontend: Frontend) -> Elab {
-        todo!();
+        let design: *mut Yosys_RTLIL_Design = unsafe { new_yosys_rtlil_design() };
+        unsafe { Yosys_yosys_setup(); }
+
+        let frontend_command: *mut c_char = str_to_cstr(frontend.get_frontend_command());
+        let file_path: *mut c_char = str_to_cstr(path);
+
+        unsafe { Yosys_run_frontend_wrapper(file_path, frontend_command, design) };
+        
+        Elab { yosys_rtlil_design: design }
     }
 }
--- src/frontend.rs
+++ src/frontend.rs
@@ -4,10 +4,23 @@ use std::path::PathBuf;
 
 pub struct Frontend {
     sv_files: Vec<PathBuf>,
+    frontend_command: String,
 }
 
 impl Frontend {
     pub fn new(sv_files: Vec<PathBuf>) -> Self {
-        Frontend { sv_files }
+        let frontend_command = "auto";
+        Frontend { 
+            sv_files ,
+            frontend_command: frontend_command.to_string(),
+        }
+    }
+    
+    pub fn get_sv_files(&self) -> &Vec<PathBuf> {
+        &self.sv_files
+    }
+    
+    pub fn get_frontend_command(&self) -> &str {
+        &self.frontend_command
     }
 }
